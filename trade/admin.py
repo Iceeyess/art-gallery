89b189e8abc494ae360a3749ad68d55462af34cb
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2025-09-12 19:52:21 +0300

    1) admin.py was modified as per new models. 2) footer bug '.' symbol between sentences was removed.

--- trade/admin.py
+++ trade/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from trade.models import PreOrder, Order
+from trade.models import PreOrder, Order, OrderItem, CardContact
 
 
 # Register your models here.
@@ -14,3 +14,15 @@ class PreOrderAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('pk', 'order_number', 'client_ip', 'total_amount')
     list_display_link =  ('pk', 'order_number', 'client_ip', 'total_amount')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'order', 'item', 'quantity', 'price')
+    list_display_link =  ('pk', 'order', 'item', 'quantity', 'price')
+    list_filter = ('order', 'item')
+
+@admin.register(CardContact)
+class CardContactAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'order', 'name', 'email', 'address', 'created_at', 'updated_at')
+    list_display_link = ('pk', 'order', 'name', 'email', 'address', 'created_at', 'updated_at')
+    list_filter = ('order', 'name', 'email')
